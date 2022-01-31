96c11d6b1299e0463ff8ca331f81632c7e25aa61
JarousNemec <jarous.nemec@gmail.com>
2022-01-31 19:45:41 +0100

    cleaned code

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,3 @@
-//#include <Arduino.h>
-//
-//#include <ESP8266WiFi.h>
-//#include <ESP8266HTTPClient.h>
-//
-////void setup() {
-////    Serial.begin(9600);
-////    pinMode(2, OUTPUT);
-////}
-////
-////int state = false;
-////
-////void loop() {
-////    digitalWrite(2, state);
-////    state = !state;
-////    Serial.println("69");
-////    delay(1000);
-////}
-//
-//const char* ssid     = "ASUS";
-//const char* password = "hesloheslo";
-//
-//const char* host = "mfapi.westeurope.azurecontainer.io";
-//const char* streamId   = "....................";
-//const char* privateKey = "....................";
-//HTTPClient http;
-//void setup() {
-//    Serial.begin(115200);
-//    delay(10);
-//
-//    // We start by connecting to a WiFi network
-//
-//    Serial.println();
-//    Serial.println();
-//    Serial.print("Connecting to ");
-//    Serial.println(ssid);
-//
-//    WiFi.begin(ssid, password);
-//
-//    while (WiFi.status() != WL_CONNECTED) {
-//        delay(500);
-//        Serial.print(".");
-//    }
-//
-//    Serial.println("");
-//    Serial.println("WiFi connected");
-//    Serial.println("IP address: ");
-//    Serial.println(WiFi.localIP());
-//      //Declare an object of class HTTPClient
-//WiFiClient wiFiClient;
-//    http.begin(wiFiClient,"http://mfapi.westeurope.azurecontainer.io:8081/allresults");
-//}
-//
-//
-//void loop() {
-//    if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
-//
-//         //Specify request destination
-//        int httpCode = http.GET();                                  //Send the request
-//
-//        if (httpCode > 0) { //Check the returning code
-//
-//            String payload = http.getString();   //Get the request response payload
-//            Serial.println(payload);             //Print the response payload
-//
-//        }
-//
-//        http.end();   //Close connection
-//
-//    }
-//}
-
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
@@ -84,7 +12,6 @@ WiFiClient wiFiClient;
 String PrepateToParseJson(String json);
 
 String GetValueFromString(String data, const String& findingKey);
-
 
 void setup() {
     Serial.begin(115200);
@@ -154,4 +81,3 @@ String GetValueFromString(String data, const String& findingKey) {
 
     return "";
 }
-
